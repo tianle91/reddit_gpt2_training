fb00efbf81064d5b5bd47ea5ca2733fd2c5a03b2
tianle-mbp <tianlechen@gmail.com>
2019-09-10 20:56:03 -0400

    remove training data code and replace with some testing

--- data_reddit.py
+++ data_reddit.py
@@ -1,17 +1,15 @@
-import argparse
 import json
-import os
 
 import praw
+from praw.exceptions import ClientException
+
+QSTR = 'What do you think?'
 
 
 def get_parent_comments(comment, reddit):
-    # parent_id	The ID of the parent comment. 
-    # If it is a top-level comment, this returns the submission ID instead (prefixed with ‘t3’).
+    '''return list of [parent_comment.body]'''
     parent_id = comment.parent_id
     parent_comments = []
-    # link_id	The submission ID that the comment belongs to.
-    # recursively get parent of comment until parent_id == comment.link_id
     while parent_id != comment.link_id:
         parent_comment = reddit.comment(id=parent_id[3:])
         parent_comments.append(parent_comment.body)
@@ -19,58 +17,64 @@ def get_parent_comments(comment, reddit):
     return parent_comments
 
 
+def format_submission(submission):
+    return '\n\n'.join([
+        'In subreddit: %s' % submission.subreddit.display_name,
+        'Title: %s' % submission.title,
+        'Body: %s' % submission.selftext
+    ])
+
+
 def get_parent_submission(comment, reddit):
-    # link_id	The submission ID that the comment belongs to.
-    submission = reddit.submission(id=comment.link_id[3:])
-    # return ' '.join(submission.title.split() + submission.selftext.split())
-    return {
-        'subreddit': submission.subreddit.display_name,
-        'str': ' '.join(submission.title.split() + submission.selftext.split())
-    }
+    return reddit.submission(id=comment.link_id[3:])
 
 
 def gen_qa_string(q, a):
     return 'Q: %s\nA: %s' % (q, a)
 
 
-def gen_formatted_as_qa(comment, reddit):
-    parents = get_parent_comments(comment, reddit)
-    nparents = len(parents)
-    submission = get_parent_submission(comment, reddit)
-
+def gen_qa_training(comment, reddit):
     # https://openai.com/blog/better-language-models/#task1
     # format the context/paragraph
-    sl = ['In subreddit: %s' % submission['subreddit'], submission['str']]
+    sl = [format_submission(get_parent_submission(comment, reddit))]
+    comments_chain = [comment.body] + get_parent_comments(comment, reddit) + [QSTR]
+    for i in range(len(comments_chain) - 1, 0, -1):
+        sl.append(gen_qa_string(comments_chain[i], comments_chain[i - 1]))
+    return '\n\n'.join(sl)
 
-    if nparents > 1:
-        for i in range(1, nparents):
-            sl.append(gen_qa_string(parents[i - 1], parents[i]))
-    else:
-        # default, if top-level comment
-        qstr = 'What do you think?'
-        if nparents == 1:
-            qstr = parents[0]
-        sl.append(gen_qa_string(qstr, comment.body))
+
+def gen_qa_infer(url, reddit):
+    try:
+        comment = reddit.comment(url=url)
+        sl = [gen_qa_training(comment, reddit), gen_qa_string(comment.body, '')]
+    except ClientException:
+        submission = reddit.submission(url=url)
+        sl = [format_submission(submission), gen_qa_string(QSTR, '')]
     return '\n\n'.join(sl)
 
 
 if __name__ == '__main__':
-    # spez BennyFeldman whiskeysquid
-    parser = argparse.ArgumentParser(description='dump user comments')
-    parser.add_argument('-user', default='spez', type=str)
-    parser.add_argument('-num-comments', default=100, type=int)
-    args = parser.parse_args()
-
     with open('praw.cred') as f:
         r = praw.Reddit(**json.load(f))
-        # print('API state\tread only: %s, user: %s' % (r.read_only, r.user.me()))
 
-    u = r.redditor(args.user)
-    comments_gen = u.comments.new(limit=args.num_comments)
-    with open(os.path.join('data_reddit', '%s.txt' % u.name), 'a+') as f:
-        i = 0
-        print('getting %d new comments for user: %s' % (args.num_comments, args.user))
-        for c in comments_gen:
-            print('[%d/%d] parsing comment id: %s' % (i, args.num_comments, c.id))
-            f.write(gen_formatted_as_qa(c, r) + '\n'*2)
-            i += 1
+    breakstr = '-' * 100
+    print(breakstr)
+    print('test gen_qa_training on comments...')
+    print(breakstr)
+    u = r.redditor('spez')
+    comments_gen = u.comments.new(limit=2)
+    for c in comments_gen:
+        print(c.id)
+        print(gen_qa_training(c, r))
+
+    print(breakstr)
+    print('test gen_qa_infer on submission...')
+    print(breakstr)
+    print(gen_qa_infer(
+        'https://www.reddit.com/r/AskReddit/comments/d2anfy/how_are_you_doing_today_you_sexy_mother_fucker/', r))
+
+    print(breakstr)
+    print('test gen_qa_infer on comment...')
+    print(breakstr)
+    print(gen_qa_infer(
+        'https://www.reddit.com/r/AskReddit/comments/d2anfy/how_are_you_doing_today_you_sexy_mother_fucker/eztp4fx', r))
