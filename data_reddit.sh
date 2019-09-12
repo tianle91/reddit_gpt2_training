ca73dc6e0346ff102e55669cea98f77a0cca8ea3
tianle-mbp <tianlechen@gmail.com>
2019-09-12 08:30:22 -0400

    get and pack training

--- data_reddit.sh
+++ data_reddit.sh
@@ -1,4 +1,5 @@
 for u in spez BennyFeldman whiskeysquid
 do
-	nohup python -u get_training.py -user $u -num-comments 1000 >"log/$u.log" &
+	python -u get_training.py -user $u -num-comments 1000 >"log/$u.log"
+	python pack_training.py -user $u
 done
