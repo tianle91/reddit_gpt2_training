7c447dfc5df601470f069fa9fc2555e18536a4bc
tianle-mbp <tianlechen@gmail.com>
2019-09-11 23:39:09 -0400

    write individual comments to file

--- get_training.py
+++ get_training.py
@@ -17,13 +17,22 @@ with open('praw.cred') as f:
 
 u = r.redditor(args.user)
 comments_gen = u.comments.new(limit=args.num_comments)
-with open(os.path.join('data_reddit', '%s.txt' % u.name), 'a+') as f:
-    i = 0
-    print('getting %d new comments for user: %s' % (args.num_comments, args.user))
-    for c in comments_gen:
-        s = gen_qa_training(c, r)
-        print('[%d/%d] | comment id: %s | tail: %s |' %\
-              (i, args.num_comments, c.id, ' '.join(s.split())[-20:]))
-        f.write(s + '\n'*2)
-        i += 1
-    print('done!')
+writedir = os.path.join('data_reddit', u.name)
+if not os.path.isdir(writedir):
+    os.mkdir(writedir)
+
+i = 0
+print('getting %d new comments for user: %s' % (args.num_comments, args.user))
+for c in comments_gen:
+    writepath = os.path.join(writedir, '%s.txt' % c.id)
+    if not os.path.isfile(writepath):
+        with open(writepath, 'w') as f:
+            s = gen_qa_training(c, r)
+            f.write(s)
+            print('[%d/%d] | comment id: %s | tail: %s' %\
+                  (i, args.num_comments, c.id, ' '.join(s.split())[-20:]))
+    else:
+        print('[%d/%d] | comment id: %s | exists!' % \
+              (i, args.num_comments, c.id))
+    i += 1
+print('done!')
